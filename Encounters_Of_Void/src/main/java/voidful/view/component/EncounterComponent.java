5fe604bd09b0003f05453579677449c029bbbe4e
JarlNuramon <saiedsadeghz@gmail.com>
2019-10-08 14:12:06 +0200

    Unnessecary Import removed

--- Encounters_Of_Void/src/main/java/voidful/view/component/EncounterComponent.java
+++ Encounters_Of_Void/src/main/java/voidful/view/component/EncounterComponent.java
@@ -3,7 +3,6 @@ package voidful.view.component;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import voidful.entity.session.EncounterEntity;
 
